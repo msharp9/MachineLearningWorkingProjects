b4142491b8fdfa90422a5300c9941bd5d68be2df
msharp9 <msharp9@outlook.com>
2018-03-17 17:26:15 -0600

    Created initial_population function to build a db of winning games

--- CartPole/cartpole.py
+++ CartPole/cartpole.py
@@ -12,7 +12,7 @@ env = gym.make("CartPole-v0")
 env.reset()
 goal_steps = 500
 score_requirement = 50
-initial_games = 10000
+initial_games = 10000 #100 #2
 
 def some_random_games_first():
     # Each of these is its own game.
@@ -36,73 +36,77 @@ def some_random_games_first():
             if done:
                 break
 
-some_random_games_first()
+# some_random_games_first()
 
-# def initial_population():
-#     # [OBS, MOVES]
-#     training_data = []
-#     # all scores:
-#     scores = []
-#     # just the scores that met our threshold:
-#     accepted_scores = []
-#     # iterate through however many games we want:
-#     for _ in range(initial_games):
-#         score = 0
-#         # moves specifically from this environment:
-#         game_memory = []
-#         # previous observation that we saw
-#         prev_observation = []
-#         # for each frame in 200
-#         for _ in range(goal_steps):
-#             # choose random action (0 or 1)
-#             action = random.randrange(0,2)
-#             # do it!
-#             observation, reward, done, info = env.step(action)
+def initial_population():
+    # [OBS, MOVES]
+    training_data = []
+    # all scores:
+    scores = []
+    # just the scores that met our threshold:
+    accepted_scores = []
+    # iterate through however many games we want:
+    for _ in range(initial_games):
+        score = 0
+        # moves specifically from this environment:
+        game_memory = []
+        # previous observation that we saw
+        prev_observation = []
+        # for each frame in 200
+        for _ in range(goal_steps):
+            # choose random action (0 or 1)
+            action = random.randrange(0,2)
+            # do it!
+            observation, reward, done, info = env.step(action)
+            # print(observation, reward, done, info)
+
+            # notice that the observation is returned FROM the action
+            # so we'll store the previous observation here, pairing
+            # the prev observation to the action we'll take.
+            if len(prev_observation) > 0 :
+                game_memory.append([prev_observation, action])
+            prev_observation = observation
+            score+=reward
+            if done: break
+
+        # print(score)
+        # IF our score is higher than our threshold, we'd like to save
+        # every move we made
+        # NOTE the reinforcement methodology here.
+        # all we're doing is reinforcing the score, we're not trying
+        # to influence the machine in any way as to HOW that score is
+        # reached.
+        if score >= score_requirement:
+            accepted_scores.append(score)
+            for data in game_memory:
+                # convert to one-hot (this is the output layer for our neural network)
+                if data[1] == 1:
+                    output = [0,1]
+                elif data[1] == 0:
+                    output = [1,0]
+
+                # saving our training data
+                training_data.append([data[0], output])
+            # print(game_memory)
+            # print(training_data)
+
+        # reset env to play again
+        env.reset()
+        # save overall scores
+        scores.append(score)
+
+    # just in case you wanted to reference later
+    training_data_save = np.array(training_data)
+    np.save('CartPole/output/saved.npy',training_data_save)
+
+    # some stats here, to further illustrate the neural network magic!
+    print('Average accepted score:',mean(accepted_scores))
+    print('Median score for accepted scores:',median(accepted_scores))
+    print(Counter(accepted_scores))
+
+    return training_data
 #
-#             # notice that the observation is returned FROM the action
-#             # so we'll store the previous observation here, pairing
-#             # the prev observation to the action we'll take.
-#             if len(prev_observation) > 0 :
-#                 game_memory.append([prev_observation, action])
-#             prev_observation = observation
-#             score+=reward
-#             if done: break
-#
-#         # IF our score is higher than our threshold, we'd like to save
-#         # every move we made
-#         # NOTE the reinforcement methodology here.
-#         # all we're doing is reinforcing the score, we're not trying
-#         # to influence the machine in any way as to HOW that score is
-#         # reached.
-#         if score >= score_requirement:
-#             accepted_scores.append(score)
-#             for data in game_memory:
-#                 # convert to one-hot (this is the output layer for our neural network)
-#                 if data[1] == 1:
-#                     output = [0,1]
-#                 elif data[1] == 0:
-#                     output = [1,0]
-#
-#                 # saving our training data
-#                 training_data.append([data[0], output])
-#
-#         # reset env to play again
-#         env.reset()
-#         # save overall scores
-#         scores.append(score)
-#
-#     # just in case you wanted to reference later
-#     training_data_save = np.array(training_data)
-#     np.save('saved.npy',training_data_save)
-#
-#     # some stats here, to further illustrate the neural network magic!
-#     print('Average accepted score:',mean(accepted_scores))
-#     print('Median score for accepted scores:',median(accepted_scores))
-#     print(Counter(accepted_scores))
-#
-#     return training_data
-#
-#     def neural_network_model(input_size):
+#def neural_network_model(input_size):
 #
 #     network = input_data(shape=[None, input_size, 1], name='input')
 #
@@ -139,7 +143,7 @@ some_random_games_first()
 #     model.fit({'input': X}, {'targets': y}, n_epoch=5, snapshot_step=500, show_metric=True, run_id='openai_learning')
 #     return model
 #
-# training_data = initial_population()
+training_data = initial_population()
 #
 # model = train_model(training_data)
 #
