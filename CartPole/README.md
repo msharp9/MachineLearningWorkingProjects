e772728f0094e21b227077ab7035e5d528541965
msharp9 <msharp9@outlook.com>
2018-03-17 19:18:08 -0600

    Training and testing models, duplicating results

--- CartPole/README.md
+++ CartPole/README.md
@@ -1,1 +1,4 @@
 Following a tutorial on pythonprogramming.net wanted to get more familiar with reinforcement learning and gym.  CartPole appears to be the hello world of this subspace.
+
+The best I could do was 200 seconds exactly, got it several times.  The tutorial claimed to get almost twice that, so I'm wondering if there's a difference in versions.
+The answer was yes, my gym is a newer version and I needed to be running on CarPole-v1.  After updating I was easily able to duplicate results.
--- CartPole/cartpole.py
+++ CartPole/cartpole.py
@@ -7,12 +7,13 @@ from tflearn.layers.estimator import regression
 from statistics import median, mean
 from collections import Counter
 
-LR = 1e-3
-env = gym.make("CartPole-v0")
+LR = 1e-4
+# env = gym.make("CartPole-v0")
+env = gym.make("CartPole-v1")
 env.reset()
 goal_steps = 500
-score_requirement = 50
-initial_games = 10000 #100 #2
+score_requirement = 75
+initial_games = 100000 #100 #2
 
 def some_random_games_first():
     # Each of these is its own game.
@@ -97,7 +98,7 @@ def initial_population():
 
     # just in case you wanted to reference later
     training_data_save = np.array(training_data)
-    np.save('CartPole/output/saved.npy',training_data_save)
+    np.save('output/saved.npy',training_data_save)
 
     # some stats here, to further illustrate the neural network magic!
     print('Average accepted score:',mean(accepted_scores))
@@ -143,38 +144,43 @@ def train_model(training_data, model=False):
     if not model:
         model = neural_network_model(input_size = len(X[0]))
 
-    model.fit({'input': X}, {'targets': y}, n_epoch=5, snapshot_step=500, show_metric=True, run_id='openai_learning')
+    model.fit({'input': X}, {'targets': y}, n_epoch=7, snapshot_step=500, show_metric=True, run_id='openai_learning')
     return model
 
 training_data = initial_population()
 
 model = train_model(training_data)
 
-# scores = []
-# choices = []
-# for each_game in range(10):
-#     score = 0
-#     game_memory = []
-#     prev_obs = []
-#     env.reset()
-#     for _ in range(goal_steps):
-#         env.render()
-#
-#         if len(prev_obs)==0:
-#             action = random.randrange(0,2)
-#         else:
-#             action = np.argmax(model.predict(prev_obs.reshape(-1,len(prev_obs),1))[0])
-#
-#         choices.append(action)
-#
-#         new_observation, reward, done, info = env.step(action)
-#         prev_obs = new_observation
-#         game_memory.append([new_observation, action])
-#         score+=reward
-#         if done: break
-#
-#     scores.append(score)
-#
-# print('Average Score:',sum(scores)/len(scores))
-# print('choice 1:{}  choice 0:{}'.format(choices.count(1)/len(choices),choices.count(0)/len(choices)))
-# print(score_requirement)
+
+# Testing the model
+scores = []
+choices = []
+for each_game in range(10):
+    score = 0
+    game_memory = []
+    prev_obs = []
+    env.reset()
+    for _ in range(goal_steps):
+        env.render()
+
+        if len(prev_obs)==0:
+            action = random.randrange(0,2)
+        else:
+            action = np.argmax(model.predict(prev_obs.reshape(-1,len(prev_obs),1))[0])
+
+        choices.append(action)
+
+        new_observation, reward, done, info = env.step(action)
+        prev_obs = new_observation
+        game_memory.append([new_observation, action])
+        score+=reward
+        if done: break
+
+    scores.append(score)
+
+average_score = sum(scores)/len(scores)
+print('Average Score:',average_score)
+print('choice 1:{}  choice 0:{}'.format(choices.count(1)/len(choices),choices.count(0)/len(choices)))
+print(score_requirement)
+
+model.save('output/{}.model'.format(average_score))
